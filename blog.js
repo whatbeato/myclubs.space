266f1993a843eb93dae70bdc766ea4b590b5195a
fonz beato <96067139+whatbeato@users.noreply.github.com>
2025-10-08 12:26:04 +0100

    i lowk forgot to commit

--- blog.js
+++ blog.js
@@ -1,11 +1,14 @@
 const params = new URLSearchParams(window.location.search);
-const post = params.get("post") || "test";
-
-fetch(`news/${post}.md`)
-.then(res => res.text())
-.then(md => {
-    document.getElementById("content").innerHTML = marked.parse(md)
-})
-.catch(err => {
-    document.getElementById("content").innerHTML = `<p>something happened when loading the post :c - ${err}</p>`
-})
+const post = params.get("post")
+if(!post) {
+    document.getElementById("content").innerHTML = "<p>select a post :3</p>";   
+} else {
+    fetch(`news/${post}.md`)
+    .then(res => res.text())
+    .then(md => {
+        document.getElementById("content").innerHTML = marked.parse(md)
+    })
+    .catch(err => {
+        document.getElementById("content").innerHTML = `<p>something happened when loading the post :c - ${err}</p>`
+    });
+}
